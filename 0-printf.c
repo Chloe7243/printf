ec3185344797d9e01df9c99c1bcc66b89112ef12
0xUgochukwu <0xUgochukwu@gmail.com>
2023-03-25 13:15:54 -0700

    helper functions

---
+++ 0-printf.c
@@ -0,0 +1,8 @@
+#include "printf.h"
+
+
+/**
+ * _printf - print stuff in c
+ * @format: printing format
+ */
+int _printf(const char *format, ...)
---
+++ putchar.c
@@ -0,0 +1,12 @@
+#include "printf.h"
+
+/**
+ * _putchar - write a character to standard output stream
+ * @c: character to be printed
+ * Return: integer
+ */
+
+int _putchar(char c)
+{
+	return (write(STDOUT_FILENO, &c, 1));
+}
