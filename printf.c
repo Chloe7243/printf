8256520c1b4e448df72829a2741d2c65c31ef6a8
0xUgochukwu <0xUgochukwu@gmail.com>
2023-03-26 11:52:31 -0700

    test

--- printf.c
+++ printf.c
@@ -1,38 +1,65 @@
 #include "main.h"
 
-/**
- * _printf - advanced replica of printf function
- *
- * @format: string input
- *
- * Return: length of output string
- */
 
+/**
+ * _printf - Produces output according to a format
+ *
+ * @format: A character string containing zero or more directives
+ *          to produce output
+ * @...: A variable number of arguments to be printed according to
+ *       the format
+ *
+ * Return: The number of characters printed (excluding the null byte
+ *         used to end output to strings)
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i, len, outlen = 0;
+	int printed = 0;
+	char c;
+	char *str;
 
 	va_start(args, format);
-	len = strlen(format);
 
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-
-	for (i = 0; i < len; i++)
+	while (*format)
 	{
-		if (format[i] == '%')
+		if (*format == '%')
 		{
-			i++;
-			outlen += get_printf(format[i], args);
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					c = (char)va_arg(args, int);
+					printed += write(1, &c, 1);
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str == NULL)
+						str = "(null)";
+					while (*str)
+					{
+						printed += write(1, str, 1);
+						str++;
+					}
+					break;
+				case '%':
+					printed += write(1, "%", 1);
+					break;
+				default:
+					printed += write(1, "%", 1);
+					printed += write(1, &(*format), 1);
+					break;
+			}
 		}
 		else
-			outlen += _putchar(format[i]);
+		{
+			printed += write(1, &(*format), 1);
+		}
+
+		format++;
 	}
 
 	va_end(args);
 
-	return (outlen);
+	return (printed);
 }
