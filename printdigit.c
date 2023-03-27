309915a37269ffa54aa0e0d2fce357915e44df48
Stephanie <Stephaniechloe21@gmail.com>
2023-03-27 04:26:22 -0700

    Task 1

--- printdigit.c
+++ printdigit.c
@@ -12,19 +12,22 @@
 int print_digit(int num)
 {
 	int count = 0;
+	unsigned int n;
 
 	if (num < 0)
 	{
-		num = -num;
+		n = -num;
 		count += _putchar('-');
 	}
+	else
+		n = num;
 
-	if (num >= 10)
+	if (n >= 10)
 	{
-		print_digit(num / 10);
+		print_digit(n / 10);
 	}
 
-	count += _putchar('0' + (num % 10));
+	count += _putchar('0' + (n % 10));
 
 	return (count);
 }
