4d79f4ca4e037e0578683810b348a6f88ef14dab
0xUgochukwu <0xUgochukwu@gmail.com>
2023-03-27 23:01:05 -0700

    binary fix

--- getprintf.c
+++ getprintf.c
@@ -35,7 +35,7 @@ int get_printf(char c, va_list args)
 			break;
 		case 'b':
 			digit = va_arg(args, unsigned int);
-			rval += (count_digit(digit, 2) - 1);
+			rval += (count_digit(digit, 2));
 			print_binary(digit);
 			break;
 		default:
